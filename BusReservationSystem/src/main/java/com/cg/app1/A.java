63c93adf83ffed2fe56633d8939815ba2ced624b
DELL <DELL@DESKTOP-SI83S6J>
2022-01-06 22:38:10 +0530

    class A is inserted

---
+++ BusReservationSystem/src/main/java/com/cg/app1/A.java
@@ -0,0 +1,10 @@
+package com.cg.app1;
+
+public class A {
+	
+	public void fun()
+	{
+		System.out.println("inside class of A");
+	}
+
+}
--- BusReservationSystem/src/main/java/com/cg/app1/projectA.java
+++
@@ -1,9 +0,0 @@
-package com.cg.app1;
-
-public class projectA {
-	
-	public void ticket() {
-		System.out.println("hiiiii");
-	}
-
-}
