7b41883986299c2cc6186a6ae95664114c1d4e93
DELL <DELL@DESKTOP-SI83S6J>
2022-01-07 12:29:24 +0530

    i have added lombok

---
+++ BusReservationSystem/src/main/java/com/cg/app1/controller/Admin.java
@@ -0,0 +1,26 @@
+package com.cg.app1.controller;
+
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+
+
+
+@Getter
+@Setter
+public class Admin {
+	
+	//@Id
+	//@GeneratedValue(strategy=GenerationType.Auto)
+	private Integer adminId;
+	private String  adminUserName;
+	private String  adminPassword;
+	
+	
+	
+	
+	
+
+}
