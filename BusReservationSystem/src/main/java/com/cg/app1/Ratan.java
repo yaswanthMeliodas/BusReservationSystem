3e63ef030469870b6d2a80e4cfdc27d942a72dc9
Ratan <ratankapr22@gmail.com>
2022-01-07 10:07:49 +0530

    commit from ratan

--- BusReservationSystem/src/main/java/com/cg/app1/Ratan.java
+++ BusReservationSystem/src/main/java/com/cg/app1/Ratan.java
@@ -1,8 +1,13 @@
 package com.cg.app1;
 
 public class Ratan {
+
 	public void fun()
 	{
 		System.out.println("inside class of A");
+	}
+	
+	public static void main(String[] args) {
+		new Ratan().fun();
 	}
 }
